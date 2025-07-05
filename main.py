f6a394b6004607754158ace951d771454a433bf0
Nick <nicholasakho@gmail.com>
2025-07-05 23:42:26 +1200

    Implemented a prototype version of automatica video/audio transcription

--- main.py
+++ main.py
@@ -1,1 +1,63 @@
-print("test")
+import moviepy.editor as mp
+from moviepy.video.io.VideoFileClip import VideoFileClip
+from pydub import AudioSegment
+from pydub.utils import make_chunks
+import speech_recognition as sr
+import os
+
+def generateAudio(filePath):
+    video = mp.VideoFileClip(filePath)
+
+    audioFile = video.audio
+    audioFile.write_audiofile("audio.wav")
+
+    audio = AudioSegment.from_file("audio.wav")
+    chunkMS = 60 * 1000 # 60 Seconds
+    chunks = make_chunks(audio, chunkMS)
+    return chunks
+
+filePath = "COMP261_2025_Lecture on 23_05_2025 (Fri)_default_054980a2.mp4"
+
+print(filePath)
+
+
+
+audio = generateAudio(filePath)
+
+os.makedirs("audioChunks", exist_ok=True)
+
+print("initlaizing recognizer")
+recognizer = sr.Recognizer()
+
+with open("Transcript_"+filePath+".txt", "w", buffering=1) as file:
+    print("Opened file")
+    for i, chunk in enumerate(audio):
+        chunk_filename = f"audioChunks_{i}.wav"
+        chunk_path = os.path.join("audioChunks", chunk_filename)
+        chunk.export(chunk_path, format="wav")
+
+        with sr.AudioFile(chunk_path) as source:
+            audio_data = recognizer.record(source)
+
+            try:
+                text = recognizer.recognize_google(audio_data)
+                print(f"Chunk {i}: {text}")
+                file.write("Minute"+i+": "+text+"\n")
+            except sr.UnknownValueError:
+                print(f"Chunk {i}: Could not understand audio / No Audio")
+            except sr.RequestError as e:
+                print(f"Chunk {i}: Could not request results from Google - API error")
+
+
+
+"""
+print("getting source")
+with sr.AudioFile("audio.wav") as source:
+    data = recognizer.record(source)
+
+print("Googling source data")
+text = recognizer.recognize_google(data)
+
+print(text)
+
+"""
